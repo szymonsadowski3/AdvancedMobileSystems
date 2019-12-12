044c5a5640d92b32bf7afb672b7022810c746df5
Daniel Mynarski <daniel.mynarski@ge.com>
2019-12-12 14:14:36 +0100

    Initial Commit

---
+++ helloMobile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  helloMobile
+//
+//  Created by Daniel Mynarski on 12/12/2019.
+//  Copyright © 2019 Daniel Mynarski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
